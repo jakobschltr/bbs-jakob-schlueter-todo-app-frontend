39e74f8f218a3e6714931ede6238682c002b4758
Jakob Schlüter <j.schlueter@basecom.de>
2026-03-26 05:14:10 +0100

    Add Nuxt v4 Project with Tailwind via vite

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    Todo List
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="bg-primary">Aufgaben Liste</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
