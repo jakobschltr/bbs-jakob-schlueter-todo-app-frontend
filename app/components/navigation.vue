ec6da5fe85a29803bf7f21d8895793329e83b4bd
Jakob Schlüter <j.schlueter@basecom.de>
2026-03-28 16:59:10 +0100

    Add basic Structure for Navigation

---
+++ app/components/navigation.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-neutral w-75 p-4">
+        <NuxtLink to="/">
+            Übersicht
+        </NuxtLink>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,37 +1,13 @@
 <template>
     <div class="flex flex-col min-h-screen">
-        <header class="bg-neutral flex justify-between p-4 items-center">
-            <h1 class="text-2xl">Mach ich morgen</h1>
-            <button
-                type="button"
-                aria-label="Farbschema umschalten"
-                class="h-6 w-6"
-                @click="colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'"
-            >
-                <ClientOnly>
-                    <Icon
-                        v-if="colorMode.value === 'light'"
-                        class="h-full w-full"
-                        name="material-symbols:light-mode-outline-rounded"
-                    />
-                    <Icon
-                        v-else
-                        class="h-full w-full"
-                        name="material-symbols:dark-mode-outline-rounded"
-                    />
-                    <template #fallback>
-                        <span class="block h-full w-full" aria-hidden="true" />
-                    </template>
-                </ClientOnly>
-            </button>
-        </header>
-    
-        <main class="flex-1 flex flex-col p-4 bg-neutral-light">
-            <slot />
+        <Header />
+        <main class="flex-1 flex">
+            <Navigation />
+            <div class="w-full flex justify-center p-4">
+                <div class="max-w-6xl w-full">
+                    <slot />
+                </div>
+            </div>
         </main>
     </div>
 </template>
-
-<script lang="ts" setup>
-const colorMode = useColorMode();
-</script>
